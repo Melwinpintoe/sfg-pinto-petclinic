5950be6cd8445cb067f8d2afee63caf3a181ae2a
Melwin Pinto <melwinpinto.tri@gmail.com>
2018-11-28 02:00:56 +0530

    Create Multi-Module for Data Module, Multi module - closes #2

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpintopetclinic/SfgPintoPetclinicApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpintopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPintoPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPintoPetclinicApplication.class, args);
+    }
+}
