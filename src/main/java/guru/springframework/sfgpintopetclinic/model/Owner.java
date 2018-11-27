a0a6a8c7a9bc2be4261ab7c0d7fa52c218bfddd8
Melwin Pinto <melwinpinto.tri@gmail.com>
2018-11-28 01:59:50 +0530

    Create Multi-Module for Data Module, Multi module - closes #1

--- src/main/java/guru/springframework/sfgpintopetclinic/model/Owner.java
+++
@@ -1,4 +0,0 @@
-package guru.springframework.sfgpintopetclinic.model;
-
-public class Owner extends Person {
-}
--- src/main/java/guru/springframework/sfgpintopetclinic/model/Vet.java
+++
@@ -1,4 +0,0 @@
-package guru.springframework.sfgpintopetclinic.model;
-
-public class Vet extends Person {
-}
